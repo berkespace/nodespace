c830634cba837f3b2e8e03d9c49b33cb5169fdf7
berkespace <stelberke@gmail.com>
2022-07-06 01:42:03 +0300

    Added Queries

--- expressDemo.js
+++ expressDemo.js
@@ -2,24 +2,27 @@ var express = require('express')
 var app = express()
 
 
-app.get('/',function(request,response){
+app.get('/', function (request, response) {
     response.send('Hello Express.js')
 });
-app.get('/admin',function(request,response){
+app.get('/admin', function (request, response) {
     response.send('Admin')
 });
 
-app.get('/product',function(request,response){
+app.get('/product', function (request, response) {
+    console.log('ID:'+ request.query.id)
+    console.log('Category ID : '+ request.query.categoryid) 
     response.send('Products')
 });
 
-app.post('/product',function(request,response){
- response.send('Post request sent')
+app.post('/product', function (request, response) {
+    console.log(request.body)
+    response.send('Post request sent')
 });
 
-app.delete('/product',function(request,response){
+app.delete('/product', function (request, response) {
     response.send('Delete request sent')
-   });
+});
 
 
 var server = app.listen(8080);
