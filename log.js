fef59210a797312bed6442c32647a4c8721c83b6
soultellegend <63239999+soultellegend@users.noreply.github.com>
2022-09-01 01:20:22 +0300

    Log düzeltim
    Sıkıntıdan ekleme yaptım ve düzeltmeler

--- log.js
+++ log.js
@@ -1,10 +1,18 @@
-var log = {
-    information:function(info){
-        console.log("Information : "+info)
-    },
-    fault:function(fault){
-        console.log("Fault : "+fault)
-    }
+const chalk = require("chalk"); //npm install chalk@4.1.2
+
+const log = {
+  info: function (content) {
+    console.log(`${chalk.blueBright("INFO:")} ${content}`);
+  },
+  error: function (content) {
+    console.log(`${chalk.redBright("ERROR:")} ${content}`);
+  },
+  success: function (content) {
+    console.log(`${chalk.greenBright("SUCCESS:")} ${content}`);
+  },
+  log: function (content) {
+    console.log(`${chalk.gray("LOG:")} ${content}`);
+  },
 };
 
-module.exports = log
+module.exports = log;
